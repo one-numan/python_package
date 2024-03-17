21507f8bb9e60fa75b8e0ccdd5242d1c8bdf4086
one-numan <numamk786@gmail.com>
2024-03-18 00:49:02 +0530

    Create Python Package | Folder that contain

---
+++ python_sample_package/module1.py
@@ -0,0 +1,9 @@
+"""This is Modules 1"""
+
+
+def cal_square(number):
+    return number**2
+
+
+def doubler(number):
+    return number*2
---
+++ python_sample_package/module2.py
@@ -0,0 +1,5 @@
+""" This is Modules 2"""
+
+
+def mean(numbers):
+    return sum(numbers)/len(numbers)
